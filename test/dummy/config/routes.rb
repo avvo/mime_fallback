26d01294dd80c614c810c407c1d1d3dc6999a130
Jeff Ching <ching.jeff@gmail.com>
2014-10-18 21:43:44 -0700

    failing test for respond_to

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,4 +1,5 @@
 Dummy::Application.routes.draw do
   get 'test/basic', :to => 'test#basic'
   get 'test/responding', :to => 'test#responding'
+  get 'test/mobile_only', :to => 'test#mobile_only'
 end
