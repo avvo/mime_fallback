f8dde1a9ce6fd441d34dcb83bb3d2a598b28ea6d
Jacob Kemp <jkemp@avvo.com>
2016-03-15 21:42:07 -0700

    Updated to support new rails security fix
    See https://github.com/rails/rails/issues/23243

--- lib/mime_fallback/resolver.rb
+++ lib/mime_fallback/resolver.rb
@@ -3,16 +3,16 @@ module MimeFallback
 
     private
 
-    def find_templates(name, prefix, partial, details)
+    def find_templates(name, prefix, partial, details, outside_app_allowed = false)
       format = Array(details[:formats]).first
       fallbacks ||= MimeFallback::Type.fallbacks(format).dup
       if fallbacks.present?
         fallback_details = details.dup
         fallback_details[:formats] = Array(format.to_sym) + fallbacks.map(&:to_sym)
         path = build_path(name, prefix, partial)
-        query(path, fallback_details.select {|key| EXTENSIONS.include?(key)}, fallback_details[:formats])
+        query(path, fallback_details.select {|key| EXTENSIONS.include?(key)}, fallback_details[:formats], outside_app_allowed)
       else
-        super(name, prefix, partial, details)
+        super(name, prefix, partial, details, outside_app_allowed)
       end
       
     end
